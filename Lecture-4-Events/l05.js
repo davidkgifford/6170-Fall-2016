71913bf48a2d35a26f0db66272cb67a8ae7f6145
David Gifford <gifford@mit.edu>
2016-09-18 15:37:43 -0400

    Star Example for 6.170

---
+++ Lecture-4-Events/l05.js
@@ -0,0 +1,34 @@
+// Miscellaneous support code for Lecture 5
+
+// Functional to create a new length-'count' array,
+// by calling a function that many times, each time passing it the current index.
+Array.create = function(f, count) {
+    var arr = [];
+
+    for (var i = 0; i < count; ++i) {
+        arr.push(f(i));
+    }
+
+    return arr;
+}
+
+// Call a function on every data value in an array, in order.
+Array.prototype.each = function(f) {
+    for (var i = 0; i < this.length; i += 1) {
+        f(this[i]);
+    }
+};
+
+// Call a function on every index and data value in an array, in order.
+Array.prototype.eachi = function(f) {
+    for (var i = 0; i < this.length; i += 1) {
+        f(i, this[i]);
+    }
+};
+
+// Step through an array in order, using a function to combine elements,
+// beginning with an initial accumulator.
+Array.prototype.reduce = function(f, acc) {
+    this.each(function(x) { acc = f(x, acc); });
+    return acc;
+};
